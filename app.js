0bbcb561807c6dd85e15b0a36e1c3f78f98f9cda
Covee <uniqtop91@gmail.com>
2018-10-06 23:54:35 +0900

    fixed little error of type id & adding item to list 1

--- app.js
+++ app.js
@@ -12,6 +12,53 @@ Controller Module
 
 // budget controller
 var budgetController = (function() {
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+            
+
+            if (type === 'expense') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'income') {
+                newItem = new Income(ID, des, val);
+            }
+
+            data.allItems[type].push(newItem);
+            return newItem;
+
+        },
+        testing: function() {
+            console.log(data);
+        }
+    };
 
 })();
 
@@ -32,6 +79,19 @@ var UIController = (function() {
                 value       : document.querySelector(DOMstrings.inputValue).value
             };  
         },
+        addListItem: function(obj, type) {
+            var html;
+
+            if (type === 'income') {
+                html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            } else if (type === 'expense') {
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+            
+
+            
+        },
+
         getDOMstrings: function() {     // pass DOMstrings to the app controller
             return DOMstrings;
         }
@@ -54,10 +114,12 @@ var appController = (function(BC, UC) {
     }
 
     var ctrlAddItem = function() {      // private func.
+        var input, newItem;
         // 1. get the input data
-        var input = UC.getInput();
+        input = UC.getInput();
         console.log(input);
         // 2. add item to the budgetController
+        newItem = BC.addItem(input.type, input.description, input.value);
         // 3. add the item to the UI
         // 4. calculate budget
         // 5. display the budget on UI
